cbfe37d668f44701fc11d3a96061a3b2fb3b1a0f
Beauclair <beauclairmikouasse@gmail.com>
2024-07-25 13:34:52 +0100

    ma première version

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React from 'react'
+import TicTacToe from '../components/TicTacToe'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <TicTacToe/>
+    </main>
+  )
+}
